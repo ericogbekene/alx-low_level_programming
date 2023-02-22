cc8e97a95619e0e645aa62f4f9b1f570fcc9c948
eric <ericogbekene@icloud.com>
2023-02-22 10:05:09 -0800

    Alpha correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _salpha - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * return: 1 for alphabetic character or 0 for anything else
  */
@@ -9,7 +9,7 @@
 int _isalpha(int c)
 
 {
-	if ((c >= 65 && c <= 90) || (c>= 122))
+	if ((c >= 65 && c <= 90) || (c >= 122))
 	{
 	return (1);
 	}
