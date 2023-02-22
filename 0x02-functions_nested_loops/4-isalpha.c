d74c653695bc5ad6e0fda02155e249f037588c4f
eric <ericogbekene@icloud.com>
2023-02-22 12:44:25 -0800

    alpha check must work

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
- * return: 1 for alphabetic character or 0 for anything else
+ *Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
