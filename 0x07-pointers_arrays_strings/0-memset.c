7edc415f278055bb99bf67cc540fe355611b710a
eric <ericogbekene@icloud.com>
2023-03-06 05:59:01 -0800

    Mem test correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - fill a block of memory with a specific value
  * @s: starting adress of memory to be filled
@@ -9,11 +8,11 @@
  * Return: changed array with new value for n bytes
  */
 
-char *_memset(char *s, char b, unsigned in n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (; n . 0; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
