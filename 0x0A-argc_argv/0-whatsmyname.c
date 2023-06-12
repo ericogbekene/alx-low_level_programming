8365e1bb1f09d8bcdc7eef47fde62d901daf662d
Eric Ogbekene <ericogbekene@icloud.com>
2023-06-12 14:38:40 -0700

    a program to print the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -14,11 +14,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
 
-	for (i = 0; i < argc; i++)
-	{
 	printf("The name of the program is: %s\n", argv[0]);
-	}
+	printf("The number of arguments : %d\n", argc);
+
 	return (0);
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+
+		printf("The number of arguments is :%d\n", argc);
+		printf("program name is: %s\n", argv[0]);
+
+	return (0);
+}
