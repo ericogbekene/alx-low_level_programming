23dba461874597fcbbdf048cc969dfdd49d38319
Eric Ogbekene <ericogbekene@icloud.com>
2023-06-12 15:05:28 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -15,8 +15,9 @@
 int main(int argc, char *argv[])
 {
 
+	(void) argc;
 	printf("The name of the program is: %s\n", argv[0]);
-	printf("The number of arguments : %d\n", argc);
+	/*printf("The number of arguments : %d\n", argc);*/
 
 	return (0);
 }
