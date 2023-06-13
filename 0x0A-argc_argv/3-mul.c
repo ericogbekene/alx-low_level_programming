4812dd3edc3c2a39f75e93bd19c064e9d4a07b92
Eric Ogbekene <ericogbekene@icloud.com>
2023-06-12 18:06:56 -0700

    Multiplication of argv and print argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always (0) success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = 0;
+	int num2 = 0;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+
+	return (0);
+}
