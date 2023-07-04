6da46cc470566c41b66315786805248ccda56f54
Eric Ogbekene <ericogbekene@icloud.com>
2023-07-04 05:35:03 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node- adds a new node at the beginning of a list
+ * @head: pointer to the memory of the first node
+ * @str: input string
+ *
+ * Return: adress ofnew node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+	list_t *new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+		free(new_node);
+	return (NULL);
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return(new_node);
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Creating my singly linked lists directory
