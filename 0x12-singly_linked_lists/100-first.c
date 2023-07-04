ec60ea20b617afd2f9fb652f5001f1dea094bf8b
Eric Ogbekene <ericogbekene@icloud.com>
2023-07-04 16:41:33 -0700

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* print_message - prints a  custom message before main
+*
+* Return: void
+*/
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
