88fcfa4ecb0fd3b7543d86a545c9eecab17a4ff0
Eric Ogbekene <ericogbekene@icloud.com>
2023-07-05 18:02:57 -0700

    add a node at a particular location

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - inserts node at an index location on a list
+* @head: pointer to the first node
+* @idx: index of the position to add node
+* @n: data integer
+*
+* Return: address of the new node
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new_node, *current;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	current = *head;
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (current == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+
+		current = current->next;
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
+}
