c061fd5d4beda19277d395ec53ad937927b60688
Eric Ogbekene <ericogbekene@icloud.com>
2023-07-05 18:46:56 -0700

    check for prime number and palindrome correction usimg recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,17 @@
 
 int is_prime_number(int n)
 {
-	if (n < 2)
-		return(0);
-	int s_root = sqrt(n);
 	int i;
-       for (int i = 2; i <= s_root; i++)
-       {
-	       if (n % i == 0) 
-	       {
-		       return (0);
-	       }
-       }
-       return (1);
+
+	if (n < 2)
+		return (0);
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,5 +11,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 
 #endif
