ff0bc67d8df43189ab6f22fc418cdbab03c761aa
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-09 14:51:52 -0700

    script to create library from c files

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
- * _strcpy - copies one string src to a dest
- * @dest: destination
- * @src: source
+ * _strcpy - Copies a string pointed to by @src, including the
+ * terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: A source string to cpoy.
  *
- * Return: (0) Success
+ * Return: A pointer to the destination string @dest.
  */
-
 char *_strcpy(char *dest, char *src)
 {
+	int index = 0;
 
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+
+	return (dest);
 }
