e0cc129313bcf20a4853f743c0bc265d4d1ea516
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-11 14:45:42 -0700

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	array = (int*) malloc(sizeof(int) * (range + 1));
+	array = (int *) malloc(sizeof(int) * (range + 1));
 
 	if (array == NULL)
 		return (NULL);
