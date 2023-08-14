1bc8f921b1388840fdb97f243eae9b91d077c1fd
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-14 11:55:37 +0000

    betty style correction dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,11 +5,12 @@
  * struct dog - defines a struct data type for a dog
  * @name: name of dog
  * @age: age of dog
- *
+ * @owner: of the dog
  *
  */
 
-typedef struct dog {
+typedef struct dog
+{
 char *name;
 float age;
 char *owner;
