621157e0fea89cba741ede34ea25e6a43cc1bb03
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-14 13:11:29 +0000

    create a new dog_t

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: of new dog
+ * @age: of new dog
+ * @owner of new dog
+ *
+ * Return: new dog created
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
