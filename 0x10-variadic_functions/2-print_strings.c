4da9ef95d54a8a7e2be3cb213805bd0b8de4f751
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-17 09:41:03 -0700

    String refactoring

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,18 +3,27 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	/*char *s;*/
 	
 	va_list list;
 
 	va_start(list, n);
 
-	
+	/*s = va_arg(list, char*);*/
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(list, char*));
-		printf("%s", separator);
-	}
-	printf("\n");
+		
+		if (separator == NULL)
+			continue;
 
+		if (separator != NULL && i < (n-1))
+		{
+			printf("%s", separator);
+		}
+	}
 	va_end(list);
+
+	printf("\n");
 }
