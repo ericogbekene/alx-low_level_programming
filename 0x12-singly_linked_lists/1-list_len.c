5ff157011112f21de504e27097463fa090f2d7e2
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-25 09:36:59 +0000

    betty fix length of list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * list_len - counts the number of nodes in a list
+ * @h: pointer to the head of list
+ *
+ * Return: number of nodes
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
