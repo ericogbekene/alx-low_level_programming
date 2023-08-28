624e2894d97771ed82e67fc16da7dc90ec542f8e
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-28 17:58:43 +0000

    Styling fix for add node function

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * add_nodeint - adds a node at the beginning of a list
+ * @head: pointer to the first node on the list
+ * @n: data input for newly created node
+ *
+ * Return: pointer to the new node || NULL if failed
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode;
