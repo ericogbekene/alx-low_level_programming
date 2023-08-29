5d791915e3371d595f1603f15ec7c7c84b70a3fb
Eric Ogbekene <ericogbekene@icloud.com>
2023-08-29 13:13:41 +0000

    Betty fix for 4 free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,21 @@
 #include "lists.h"
 
+/**
+ * free_listint - frees a singly linked list
+ * @head: pointer to the first node of the list
+ *
+ * Return: void
+ */
+
 void free_listint(listint_t *head)
 {
 	listint_t *current = NULL;
 
 	if (head == NULL)
 	{
-		return ;
+		return;
 	}
-/**
-	if (head->next == NULL)
-		free(head);
-*/
+
 	else
 	{
 		current = head;
