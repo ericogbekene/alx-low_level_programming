a55a738eb95e236853f2457d55b6c8581f0228a2
Eric Ogbekene <ericogbekene@icloud.com>
2023-09-04 06:10:11 +0000

    betty style documentation for 3 setbit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
+/**
+ * set_bit - sets a bit at a particular index to 1 (on) for a given n
+ * @n: pointer to a long int (binary number
+ * @index: position to set bit
+ *
+ * Return: 1 success
+ */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	/* unsigned int i;*/
-	/* unsigned long int *result = 0; */
 	unsigned long int mask;
-
-	/* mask = malloc(sizeof(unsigned long int) * 8); */
-
-	/* check for successful malloc */
 
 	mask = 1UL << index;
 
 
-	/* if (index >= sizeof(unsigned long int) * 8)
-		return (-1); */
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
-
-		*n = *n | mask;
+	*n = *n | mask;
 
 	return (1);
-
-	/* result = n || mask; */
 }
