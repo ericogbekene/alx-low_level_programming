15ffe1fb405dc102062fb2c86d54dd6c77444e16
Eric Ogbekene <ericogbekene@icloud.com>
2023-09-04 06:48:47 +0000

    check for valid index before clearing bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,11 +12,13 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
-	mask = 1 << index;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	
+	mask = 1UL << index;
 
-	mask = ~mask;
 
-	*n = *n & mask;
+	*n = *n & ~mask;
 
 	return (1);
 
