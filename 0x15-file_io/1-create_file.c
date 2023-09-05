e4d6b6cf439cae10cbb7bb448a9af66b55b1a1cf
Eric Ogbekene <ericogbekene@icloud.com>
2023-09-05 02:28:58 +0000

    fix correct output code for create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,14 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	ssize_t output;
 	int i = 0;
 	int fd = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
 
 	if (fd == -1)
 		return (-1);
@@ -27,7 +28,14 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i] != '\0')
 		i++;
 
-	write(fd, text_content, i);
+	output = write(fd, text_content, i);
+
+	if (output == -1)
+	{
+		return (0);
+	}
+
+
 
 	close(fd);
 
