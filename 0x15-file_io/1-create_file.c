ce3069daed91cc6a07d2d708491c97e93e13225d
Eric Ogbekene <ericogbekene@icloud.com>
2023-09-05 00:16:02 +0000

    Betty fix and permission update for create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * create_file - creates a new file with r|w permissions
+ * @filename: file to be created
+ * @text_content: text to be written into the file
+ *
+ * Return: 1 on Success | -1 for error
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int i = 0;
@@ -8,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, 0644);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
