85a781ba68f6c59bbcca8808a59752007ab3cf25
Eric Ogbekene <ericogbekene@icloud.com>
2023-09-13 11:06:17 +0000

    function to print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,35 +9,22 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int j = 0, len = 0, bit = 0;
-	char *binaryvalue = NULL;
-
-	binaryvalue = malloc(sizeof(int) * n);
-	if (binaryvalue == NULL)
-		return;
+	unsigned long int j = 0;
+	int offset;
 
 	if (n == 0)
 	{
-		_putchar('0');
+		printf("0");
+		return;
 	}
 
-	while (n > 0)
+	for (j = n, offset = 0; (j >>= 1) > 0; offset++)
+		;
+
+	for (; offset >= 0; offset--)
 	{
-		bit = n & 1;
-		*binaryvalue = bit;
-		binaryvalue++;
-		n = n >> 1;
-		len++;
+		if ((n >> offset) & 1)
+			printf("1");
+		printf("0");
 	}
-	_putchar('0');
-	_putchar('b');
-
-	/* len = strlen(binaryvalue); */
-
-	for (j = (len - 1); j > 0; j--)
-	{
-		_putchar(binaryvalue[j]);
-	}
-	_putchar('\n');
-
 }
