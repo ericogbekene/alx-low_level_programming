e3127a16d6d740489b69382220c8c8c0c873a1f9
Eric Ogbekene <ericogbekene@icloud.com>
2023-10-02 08:13:43 -0700

    Betty fix for function to append to end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of file to open and append text into
+ * @text_content: content to append at the end of file
+ *
+ * Return: 1 on success or -1 on failure
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int openfile = 0, count = 0;
