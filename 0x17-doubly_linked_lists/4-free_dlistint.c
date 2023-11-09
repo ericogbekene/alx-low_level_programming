e7b0275cd76a945df2fb3ddb01992834985e8af4
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-09 15:14:30 -0800

    function to free a linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,21 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - frees a list
+ * @head: pounter to first elemet
+ *
+ */
+
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *current;
+	dlistint_t *current, *next;
 
 	current = head;
-	while (current->next != NULL)
+	while (current != NULL)
 	{
+		next = current->next;
 		free(current);
-		current = current->next;
+		current = next;
 	}
 	/*free(current);*/
 }
