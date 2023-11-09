d1e37a34122cdf5a2e75d5f33aadb5e54b13d206
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-09 14:27:09 -0800

    print elements betty fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints the elements of a llist
+ * @h: pointer to the head of the list
+ *
+ * Return: number of elements
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
