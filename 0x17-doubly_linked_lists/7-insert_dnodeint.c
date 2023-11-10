67c0ee5040e5dbc40a9fd997c686ee51170bcad4
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-10 14:19:00 -0800

    insert a node into a doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,42 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert a node at an index
- * @h: pointer to the first node
- * @idx: index to insert at
- * @n: data element to insert in list
+ * insert_dnodeint_at_index - inserts a node at index
+ * @h: pointer to head of list
+ * @idx: position to insert
+ * @n: data to insert
  *
- * Return: address of new element
+ * Return: address of inserted node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
 	dlistint_t *current;
-	dlistint_t *newNode;
+	dlistint_t *new_node;
 
-	if (h == NULL)
+	unsigned int position = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	current = *h;
+	new_node->n = n;
 
-	newNode = malloc(sizeof(dlistint_t));
-	if (newNode == NULL)
-		return (NULL);
+	current = *h; /* set current to head of list */
 
-	while (current != NULL)
+	/* move current ptr until positin b4 index */
+	while (current != NULL && position < (idx - 1))
 	{
-		if (count == (idx - 1))
-		{
-			newNode->n = n;
-			newNode->prev = current;
-			newNode->next = current->next;
-			current->next = newNode;
-
-		}
-		count++;
 		current = current->next;
+		position++;
 	}
 
-	return (newNode);
+	if (position > idx && current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = current->next;
+	new_node->prev = current;
+
+	if (current != NULL)
+	{
+		if (current->next != NULL)
+		{
+			current->next->prev = new_node;
+		}
+	}
+	current->next = new_node;
+	return (new_node);
 }
