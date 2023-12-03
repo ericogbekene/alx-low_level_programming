398bc139be9123b9b7d3320a255f0ae6c630c817
Eric Ogbekene <ericogbekene@icloud.com>
2023-12-03 15:10:54 -0800

    checking for Null input of no table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (strcmp(key, "") == 0)
 		return (0);
-
+	if (!ht || !key)
+		return (0);
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
@@ -49,6 +50,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
